be5bd98316b3d23c3f911b351d9964c13dafc13c
jhpoelen <jhpoelen@xs4all.nl>
2018-05-30 09:16:56 -0700

    attempt to address @claussni comments in https://github.com/globalbioticinteractions/elton/pull/7#issuecomment-393050848

--- src/main/java/org/globalbioticinteractions/doi/DOI.java
+++ src/main/java/org/globalbioticinteractions/doi/DOI.java
@@ -2,12 +2,16 @@ package org.globalbioticinteractions.doi;
 
 import java.io.Serializable;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Parses and presents Digital Object Identifiers (DOIs, also see <a href="https://doi.org">https://doi.org</a>).
- *
+ * <p>
  * <p>Mainly introduced to avoid encoding mistakes like mentioned in http://www.doi.org/doi_handbook/2_Numbering.html#2.5.2.3 :</p>
- *
+ * <p>
  * <p>2.5.2.3 Encoding issues</p>
  * <p>There are special encoding requirements when a DOI is used with HTML, URLs, and HTTP. The syntax for Uniform Resource Identifiers (URIs) is much more restrictive than the syntax for the DOI. A URI can be a Uniform Resource Locator (URL) or a Uniform Resource Name (URN).</p>
  * <p>Hexadecimal (%) encoding must be used for characters in a DOI that are not allowed, or have other meanings, in URLs or URNs. Hex encoding consists of substituting for the given character its hexadecimal value preceded by percent. Thus, # becomes %23 and https://doi.org/10.1000/456#789 is encoded as https://doi.org/10.1000/456%23789. The browser does not now encounter the bare #, which it would normally treat as the end of the URL and the start of a fragment, and so sends the entire string off to the DOI network of servers for resolution, instead of stopping at the #. Note that the DOI itself does not change with encoding, merely its representation in a URL. A DOI that has been encoded is decoded before being sent to the DOI Registry. At the moment the decoding is handled by the proxy server https://doi.org/. Only unencoded DOIs are stored in the DOI Registry database. For example, the number above is in the DOI Registry as "10.1000/456#789" and not "10.1000/456%23789". The percent character (%) must always be hex encoded (%25) in any URLs.</p>
@@ -16,29 +20,92 @@ import java.net.URI;
  * @see <a href="https://doi.org">https://doi.org</a>
  */
 
-public final class DOI implements Serializable {
+public final class DOI implements Serializable, Comparable<DOI> {
 
+    private static final List<String> PRINTABLE_DOI_PREFIX = Collections.singletonList("doi:");
     private final static String DIRECTORY_INDICATOR = "10";
     private final static String DIRECTORY_INDICATOR_PREFIX = DIRECTORY_INDICATOR + ".";
+    private static final String UNSECURE_DEFAULT_RESOLVER = "http://dx.doi.org/";
+    private static final String SECURE_DEFAULT_RESOLVER = "https://doi.org/";
+    private static final List<String> DOI_URLS = Arrays.asList(SECURE_DEFAULT_RESOLVER, UNSECURE_DEFAULT_RESOLVER);
+
     private final String registrantCode;
     private final String suffix;
 
+    /**
+     * @param registrantCode DOI registrant code as defined in <a href="https://www.doi.org/doi_handbook/2_Numbering.html#2.2.2">https://www.doi.org/doi_handbook/2_Numbering.html#2.2.2</a>. May not be null.
+     * @param suffix         DOI suffix as defined in <a href="https://www.doi.org/doi_handbook/2_Numbering.html#2.2.3">https://www.doi.org/doi_handbook/2_Numbering.html#2.2.3</a> . May not be null.
+     * @throws NullPointerException     on null DOI registrant code or DOI suffix.
+     * @throws IllegalArgumentException on invalid DOI registrant code or DOI suffix.
+     */
+
     public DOI(String registrantCode, String suffix) {
+        validate(registrantCode, "registrant code");
         this.registrantCode = registrantCode;
+
+        validate(suffix, "suffix");
         this.suffix = suffix;
     }
 
+    private void validate(String value, String subject) {
+        if (value == null) {
+            throw new NullPointerException("DOI " + subject + " may not be null");
+        }
+
+        if (value.trim().length() < 1) {
+            throw new IllegalArgumentException("DOI " + subject + " must contain at least one character");
+        }
+    }
+
+    private static URI URIForDoi(DOI doi) {
+        return URIForDoi(doi, URI.create(SECURE_DEFAULT_RESOLVER));
+    }
+
+    private static URI URIForDoi(DOI doi, URI resolverURI) {
+        URI uri = null;
+        URI resolv = resolverURI == null ? URI.create(SECURE_DEFAULT_RESOLVER) : resolverURI;
+        try {
+            uri = new URI(resolv.getScheme(), resolv.getHost(), "/" + doi.toString(), null);
+        } catch (URISyntaxException e) {
+            // ignore
+        }
+        return uri;
+    }
+
+    private static String stripDOIPrefix(String doi) throws MalformedDOIException {
+        for (String prefix : PRINTABLE_DOI_PREFIX) {
+            if (doi.toLowerCase().startsWith(prefix)) {
+                return doi.length() > prefix.length() ? doi.substring(prefix.length()) : doi;
+            }
+        }
+
+        for (String prefix : DOI_URLS) {
+            if (doi.length() > prefix.length() && doi.toLowerCase().startsWith(prefix)) {
+                try {
+                    String doiStripped = doi.substring(prefix.length());
+                    URI uri = URI.create("some://host/path?" + doiStripped);
+                    return uri.getQuery();
+                } catch (IllegalArgumentException e) {
+                    // some invalid characters in stripped doi - probably due to invalid url escaping
+                    // from historic doi url generator.
+                    throw new MalformedDOIException("found unescaped doi in uri [" + doi + "]", e);
+                }
+            }
+        }
+        return doi;
+    }
+
     /**
-     * Returns DOI suffix as defined in https://www.doi.org/doi_handbook/2_Numbering.html#2.2.3 :
-     *
+     * Returns DOI suffix as defined in <a href="https://www.doi.org/doi_handbook/2_Numbering.html#2.2.3">https://www.doi.org/doi_handbook/2_Numbering.html#2.2.3</a> :
+     * <p>
      * <b>2.2.3 DOI suffix</b>
-
-     <p>The DOI suffix shall consist of a character string of any length chosen by the registrant. Each suffix shall be unique to the prefix element that precedes it. The unique suffix can be a sequential number, or it might incorporate an identifier generated from or based on another system used by the registrant (e.g. ISAN, ISBN, ISRC, ISSN, ISTC, ISNI; in such cases, a preferred construction for such a suffix can be specified, as in Example 1).</p>
-
-     <b>EXAMPLE 1</b>
-     <p>10.1000/123456	DOI name with the DOI prefix "10.1000" and the DOI suffix "123456".</p>
-     <p>EXAMPLE 2</p>
-     <p>10.1038/issn.1476-4687   	DOI suffix using an ISSN. To construct a DOI suffix using an ISSN, precede the ISSN (including the hyphen) with the lowercase letters "issn" and a period, as in this hypothetical example of a DOI for the electronic version of Nature.
+     * <p>
+     * <p>The DOI suffix shall consist of a character string of any length chosen by the registrant. Each suffix shall be unique to the prefix element that precedes it. The unique suffix can be a sequential number, or it might incorporate an identifier generated from or based on another system used by the registrant (e.g. ISAN, ISBN, ISRC, ISSN, ISTC, ISNI; in such cases, a preferred construction for such a suffix can be specified, as in Example 1).</p>
+     * <p>
+     * <b>EXAMPLE 1</b>
+     * <p>10.1000/123456	DOI name with the DOI prefix "10.1000" and the DOI suffix "123456".</p>
+     * <p>EXAMPLE 2</p>
+     * <p>10.1038/issn.1476-4687   	DOI suffix using an ISSN. To construct a DOI suffix using an ISSN, precede the ISSN (including the hyphen) with the lowercase letters "issn" and a period, as in this hypothetical example of a DOI for the electronic version of Nature.
      * </p>
      *
      * @return DOI suffix
@@ -50,20 +117,20 @@ public final class DOI implements Serializable {
 
     /**
      * Returns DOI prefix as defined in <a href="https://www.doi.org/doi_handbook/2_Numbering.html#2.2.2">2.2.2 DOI prefix</a> of the DOI handbook :
-     *
+     * <p>
      * 2.2.2 DOI prefix
-
-     General
-
-     The DOI prefix shall be composed of a directory indicator followed by a registrant code. These two components shall be separated by a full stop (period).
-
-     Directory indicator
-
-     The directory indicator shall be "10". The directory indicator distinguishes the entire set of character strings (prefix and suffix) as digital object identifiers within the resolution system.
-
-     Registrant code
-
-     The second element of the DOI prefix shall be the registrant code. The registrant code is a unique string assigned to a registrant.
+     * <p>
+     * General
+     * <p>
+     * The DOI prefix shall be composed of a directory indicator followed by a registrant code. These two components shall be separated by a full stop (period).
+     * <p>
+     * Directory indicator
+     * <p>
+     * The directory indicator shall be "10". The directory indicator distinguishes the entire set of character strings (prefix and suffix) as digital object identifiers within the resolution system.
+     * <p>
+     * Registrant code
+     * <p>
+     * The second element of the DOI prefix shall be the registrant code. The registrant code is a unique string assigned to a registrant.
      *
      * @return DOI prefix
      */
@@ -74,9 +141,9 @@ public final class DOI implements Serializable {
 
     /**
      * Returns the DOI Directory Indicator. According to <a href="https://www.doi.org/doi_handbook/2_Numbering.html#2.2.2">2.2.2 DOI prefix</a> of the DOI handbook :
-     *
+     * <p>
      * <p>The directory indicator shall be "10". The directory indicator distinguishes the entire set of character strings (prefix and suffix) as digital object identifiers within the resolution system.</p>
-
+     *
      * @return directory indicator (always "10")
      */
 
@@ -86,7 +153,7 @@ public final class DOI implements Serializable {
 
     /**
      * Returns DOI Registrant Code as defined in <a href="https://www.doi.org/doi_handbook/2_Numbering.html#2.2.2">2.2.2 DOI prefix</a> of the DOI handbook :
-     *
+     * <p>
      * <p>The second element of the DOI prefix shall be the registrant code. The registrant code is a unique string assigned to a registrant.
      * </p>
      *
@@ -99,7 +166,7 @@ public final class DOI implements Serializable {
 
     /**
      * Returns printable string as defined in <a href="https://www.doi.org/doi_handbook/2_Numbering.html#2.6.1">2.6.1 Screen and print presentation</a> of the DOI handbook :
-     *
+     * <p>
      * <p>When displayed on screen or in print, a DOI name is preceded by a lowercase "doi:" unless the context clearly indicates that a DOI name is implied. The "doi:" label is not part of the DOI name value.</p>
      * <p>EXAMPLE</p>
      * <p>The DOI name "10.1006/jmbi.1998.2354" is displayed and printed as "doi:10.1006/jmbi.1998.2354".</p>
@@ -107,7 +174,7 @@ public final class DOI implements Serializable {
      * @return doi string for use in print or display
      */
 
-    public String getPrintableDOI() {
+    public String toPrintableDOI() {
         return String.format("doi:%s", this.toString());
     }
 
@@ -115,7 +182,7 @@ public final class DOI implements Serializable {
      * @return URI presentation as described in http://www.doi.org/doi_handbook/2_Numbering.html#2.6.2 using default resolver https://doi.org/
      */
     public URI toURI() {
-        return DOIUtil.URIForDoi(this);
+        return URIForDoi(this);
     }
 
     /**
@@ -124,7 +191,7 @@ public final class DOI implements Serializable {
      */
 
     public URI toURI(URI resolver) {
-        return DOIUtil.URIForDoi(this, resolver);
+        return URIForDoi(this, resolver);
     }
 
     /**
@@ -140,13 +207,13 @@ public final class DOI implements Serializable {
      * @throws MalformedDOIException on malformed DOI (e.g., 9.123/2432)
      */
     public static DOI create(String doiString) throws MalformedDOIException {
-        String s = DOIUtil.stripDOIPrefix(doiString);
+        String s = stripDOIPrefix(doiString);
         return getDOI(s);
     }
 
     /**
      * Creates a DOI from a well-formed DOI URI, decoding DOIs when necessary.
-     *
+     * <p>
      * For instance, an URI https://doi.org/10.1000/456%23789 results in a doi 10.1000/456#789 .
      *
      * @param doiURI a well-formed DOI URI
@@ -184,16 +251,6 @@ public final class DOI implements Serializable {
         return String.format("%s.%s/%s", DIRECTORY_INDICATOR, registrantCode, suffix);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return (obj != null && obj instanceof DOI)
-                && sameAs((DOI) obj);
-    }
-
-    private boolean sameAs(DOI other) {
-        return other != null && this.toString().equalsIgnoreCase(other.toString());
-    }
-
     /**
      * Utility method to check whether a prefix is commonly used for DOIs.
      *
@@ -203,7 +260,27 @@ public final class DOI implements Serializable {
 
     public static boolean isCommonlyUsedDoiPrefix(String idPrefix) {
         String prefixLower = idPrefix == null ? "" : idPrefix.toLowerCase();
-        return DOIUtil.PRINTABLE_DOI_PREFIX.contains(prefixLower) || DOIUtil.DOI_URLS.contains(prefixLower);
+        return PRINTABLE_DOI_PREFIX.contains(prefixLower) || DOI_URLS.contains(prefixLower);
     }
+
+    @Override
+    public boolean equals(Object other) {
+        return (other != null && other instanceof DOI)
+                && compareTo((DOI) other) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        result = 31 * result + (getRegistrantCode() == null ? 0 : getRegistrantCode().toLowerCase().hashCode());
+        result = 31 * result + (getSuffix() == null ? 0 : getSuffix().toLowerCase().hashCode());
+        return result;
+    }
+
+    @Override
+    public int compareTo(DOI o) {
+        return hashCode() - o.hashCode();
+    }
+
 
 }
--- src/test/java/org/globalbioticinteractions/doi/DOITest.java
+++ src/test/java/org/globalbioticinteractions/doi/DOITest.java
@@ -21,78 +21,149 @@ public class DOITest {
         assertThat(doi.getDirectoryIndicator(), is("10"));
         assertThat(doi.getRegistrantCode(), is("123"));
         assertThat(doi.getSuffix(), is("456"));
+        assertThat(doi.getPrefix(), is("10.123"));
         assertThat(doi.toString(), is("10.123/456"));
-        assertThat(doi.getPrintableDOI(), is("doi:10.123/456"));
+        assertThat(doi.toPrintableDOI(), is("doi:10.123/456"));
         assertThat(doi.toURI().toString(), is("https://doi.org/10.123/456"));
     }
 
-    @Test
-    public void fromURL() throws URISyntaxException, MalformedDOIException {
-        DOI.create(new URI("https://doi.org/10.123/456"));
-        DOI.create("doi:10.123/456");
-        DOI.create("DOI:10.123/456");
-        DOI.create("DoI:10.123/456");
+    @Test(expected = NullPointerException.class)
+    public void throwOnNullRegistrant() {
+        new DOI(null, "bla");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwOnNullSuffix() {
+        new DOI("foo", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwOnEmptySuffix() {
+        new DOI("foo", "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwOnEmptyRegistrantCodeSuffix() {
+        new DOI("", "foo");
     }
 
     @Test
-    public void create() throws MalformedDOIException, URISyntaxException {
-        DOI.create("doi:10.12/123");
+    public void createWithURI() throws URISyntaxException, MalformedDOIException {
+        URI doiURI = new URI("https://doi.org/10.123/456");
+        DOI doi = DOI.create(doiURI);
+        assertThat(doi.toURI(), is(doiURI));
     }
 
     @Test
-    public void doiFormat() throws URISyntaxException, MalformedDOIException {
-        assertThat(DOI.create("doi:10.1234/567").toString(), is("10.1234/567"));
-        assertThat(DOI.create("DOI:10.1234/567").toString(), is("10.1234/567"));
-        assertThat(DOI.create("10.1676/10-092.1").toString(), is("10.1676/10-092.1"));
-        assertThat(DOI.create("http://dx.doi.org/10.1676/10-092.1").toString(), is("10.1676/10-092.1"));
+    public void createWithDxURI() throws URISyntaxException, MalformedDOIException {
+        URI doiURI = new URI("https://example.org/10.123/456");
+        DOI doi = DOI.create(doiURI);
+        assertThat(doi.getSuffix(), is("456"));
+        assertThat(doi.getPrefix(), is("10.123"));
+        assertThat(doi.toURI(), is(URI.create("https://doi.org/10.123/456")));
     }
 
-    // see https://www.doi.org/doi_handbook/2_Numbering.html#2.2
     @Test
-    public void validDOI() throws MalformedDOIException {
-        DOI.create("10.1000/123456");
-        DOI.create("10.1038/issn.1476-4687");
+    public void createWithPrintable() throws URISyntaxException, MalformedDOIException {
+        String printableCanonical = "doi:10.123/456";
+        DOI doi = DOI.create(printableCanonical);
+        assertThat(doi.toPrintableDOI(), is(printableCanonical));
+        assertThat(doi.getPrefix(), is("10.123"));
+        assertThat(doi.getSuffix(), is("456"));
+    }
+
+    @Test
+    public void createWithPrintableCaseInsensitive() throws URISyntaxException, MalformedDOIException {
+        DOI doi = DOI.create("DOI:10.123/456");
+        assertThat(doi.toPrintableDOI(), is("doi:10.123/456"));
+    }
+
+    @Test
+    public void createWithPrintableCaseInsensitive2() throws URISyntaxException, MalformedDOIException {
+        DOI doi = DOI.create("DoI:10.123/456");
+        assertThat(doi.toPrintableDOI(), is("doi:10.123/456"));
+    }
+
+    @Test
+    public void createFromPlainDOI() throws MalformedDOIException, URISyntaxException {
+        DOI doi = DOI.create("10.1644/1545-1410(2001)683<0001:sa>2.0.co;2");
+        assertThat(doi.toPrintableDOI(), is("doi:10.1644/1545-1410(2001)683<0001:sa>2.0.co;2"));
+        assertThat(doi.getPrefix(), is("10.1644"));
+        assertThat(doi.getSuffix(), is("1545-1410(2001)683<0001:sa>2.0.co;2"));
     }
 
     @Test(expected = MalformedDOIException.class)
-    public void invalidDOIInvalidDirectoryIndicator() throws MalformedDOIException {
+    public void throwOnMalformedURI() throws MalformedDOIException, URISyntaxException {
+        DOI.create("http://dx.doi.org/10.1577/1548-8659(1993)122<0378:fotgsi>2.3.co;2");
+    }
+
+    @Test
+    public void doiToString() throws URISyntaxException, MalformedDOIException {
+        DOI doi = new DOI("123", "456");
+        assertThat(doi.toString(), is("10.123/456"));
+    }
+
+    @Test(expected = MalformedDOIException.class)
+    public void throwOnInvalidDirectoryIndicator() throws MalformedDOIException {
         DOI.create("9.1000/123456");
     }
 
     @Test(expected = MalformedDOIException.class)
-    public void invalidDOIMissingSuffix() throws MalformedDOIException {
+    public void throwOnMissingSuffix() throws MalformedDOIException {
         DOI.create("10.1038.issn.1476-4687");
     }
 
     @Test(expected = MalformedDOIException.class)
-    public void invalidDOIMissingRegistrant() throws MalformedDOIException {
+    public void throwOnMissingRegistrantAndSuffix() throws MalformedDOIException {
         DOI.create("10");
     }
 
     @Test(expected = MalformedDOIException.class)
-    public void invalidDOI() throws MalformedDOIException {
-        assertThat(DOI.create("boahhhs"), is(nullValue()));
+    public void throwOnFoo() throws MalformedDOIException {
+        DOI.create("foo");
     }
 
     @Test
-    public void toURL() throws URISyntaxException, MalformedDOIException, MalformedURLException {
-        assertThat(DOI.create("10.1000/123456").toString(), is("10.1000/123456"));
-        assertThat(DOI.create("10.1000/123#456").toURI().toString(), is("https://doi.org/10.1000/123%23456"));
-        assertThat(DOI.create("10.1206/0003-0090(2000)264<0083:>2.0.co;2").toURI().toURL().toString(), is("https://doi.org/10.1206/0003-0090(2000)264%3C0083:%3E2.0.co;2"));
+    public void toURIWithHash() throws URISyntaxException, MalformedDOIException, MalformedURLException {
+        DOI doi = DOI.create("10.1000/123#456");
+        assertThat(doi.toURI().toString(), is("https://doi.org/10.1000/123%23456"));
     }
 
     @Test
-    public void toURIForResolver() throws URISyntaxException, MalformedDOIException, MalformedURLException {
-        assertThat(DOI.create("10.1000/123456").toURI(URI.create("https://example.org")).toString(), is("https://example.org/10.1000/123456"));
+    public void toURIWithAngularBrackets() throws URISyntaxException, MalformedDOIException, MalformedURLException {
+        DOI doi = DOI.create("10.1206/0003-0090(2000)264<0083:>2.0.co;2");
+        assertThat(doi.toURI().toURL().toString(), is("https://doi.org/10.1206/0003-0090(2000)264%3C0083:%3E2.0.co;2"));
+    }
+
+    @Test
+    public void toURIWithResolver() throws URISyntaxException, MalformedDOIException, MalformedURLException {
+        DOI doi = DOI.create("10.1000/123456");
+        URI resolver = URI.create("https://example.org");
+        assertThat(doi.toURI(resolver).toString(), is("https://example.org/10.1000/123456"));
+    }
+
+    @Test
+    public void toURIWithNullResolver() throws URISyntaxException, MalformedDOIException, MalformedURLException {
+        DOI doi = DOI.create("10.1000/123456");
+        assertThat(doi.toURI(null).toString(), is("https://doi.org/10.1000/123456"));
     }
 
 
     @Test
-    public void toPrintableURL() throws URISyntaxException, MalformedDOIException {
-        assertThat(DOI.create("10.1000/123456").getPrintableDOI(), is("doi:10.1000/123456"));
-        assertThat(DOI.create("10.1000/123456").getPrintableDOI(), is("doi:10.1000/123456"));
-        assertThat(DOI.create("10.1000/123#456").getPrintableDOI(), is("doi:10.1000/123#456"));
-        assertThat(DOI.create("10.1206/0003-0090(2000)264<0083:>2.0.co;2").getPrintableDOI(), is("doi:10.1206/0003-0090(2000)264<0083:>2.0.co;2"));
+    public void toPrintable() throws URISyntaxException, MalformedDOIException {
+        assertThat(DOI.create("10.1000/123456").toPrintableDOI(), is("doi:10.1000/123456"));
+        assertThat(DOI.create("10.1000/123#456").toPrintableDOI(), is("doi:10.1000/123#456"));
+        assertThat(DOI.create("10.1206/0003-0090(2000)264<0083:>2.0.co;2").toPrintableDOI(), is("doi:10.1206/0003-0090(2000)264<0083:>2.0.co;2"));
+    }
+
+    @Test
+    public void toPrintableWithHash() throws URISyntaxException, MalformedDOIException {
+        assertThat(DOI.create("10.1000/123#456").toPrintableDOI(), is("doi:10.1000/123#456"));
+    }
+
+    @Test
+    public void toPrintableWithAngularBrackets() throws MalformedDOIException {
+        assertThat(DOI.create("10.1206/0003-0090(2000)264<0083:>2.0.co;2").toPrintableDOI(), is("doi:10.1206/0003-0090(2000)264<0083:>2.0.co;2"));
     }
 
     @Test
@@ -113,8 +184,12 @@ public class DOITest {
     }
 
     @Test
-    public void fromURL2() throws MalformedDOIException {
+    public void createFromURIWithHash() throws MalformedDOIException {
         assertThat(DOI.create("https://doi.org/10.1000/123%23456").toString(), is("10.1000/123#456"));
+    }
+
+    @Test
+    public void createFromURIWithAngularBrackets() throws MalformedDOIException {
         assertThat(DOI.create("https://doi.org/10.1206/0003-0090(2000)264%3C0083:%3E2.0.co;2").toString(), is("10.1206/0003-0090(2000)264<0083:>2.0.co;2"));
     }
 
@@ -136,7 +211,9 @@ public class DOITest {
 
     @Test
     public void whitespace() throws MalformedDOIException {
-        DOI.create("10.some/some citation");
+        DOI doi = DOI.create("10.some/some citation");
+        assertThat(doi.getSuffix(), is("some citation"));
+        assertThat(doi.toURI(), is(URI.create("https://doi.org/10.some/some%20citation")));
     }
 
     @Test
@@ -148,5 +225,27 @@ public class DOITest {
         assertThat(doi1, is(doi2));
         assertThat(doi2, is(not(doi3)));
         assertThat(doi1, is(not(doi3)));
+    }
+
+    @Test
+    public void hashCaseInsensitive() throws MalformedDOIException {
+        DOI doi1 = DOI.create("https://doi.org/10.1/ABC");
+        DOI doi2 = DOI.create("https://doi.org/10.1/AbC");
+        DOI doi3 = DOI.create("https://doi.org/10.1/AbCD");
+
+        assertThat(doi1.hashCode(), is(doi2.hashCode()));
+        assertThat(doi2.hashCode(), is(not(doi3.hashCode())));
+        assertThat(doi1.hashCode(), is(not(doi3.hashCode())));
+    }
+
+    @Test
+    public void compareToCaseInsensitive() throws MalformedDOIException {
+        DOI doi1 = DOI.create("https://doi.org/10.1/ABC");
+        DOI doi2 = DOI.create("https://doi.org/10.1/AbC");
+        DOI doi3 = DOI.create("https://doi.org/10.1/AbCD");
+
+        assertThat(doi1.compareTo(doi2), is(0));
+        assertThat(doi2.compareTo(doi3), is(not(0)));
+        assertThat(doi1.compareTo(doi3), is(not(0)));
     }
 }
